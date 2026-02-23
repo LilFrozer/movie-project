75dbc5387148ce44979c5bbf912c41d68159cd8c
Алексей Подоплелов <alekseypodoplelov@Aleksejs-MacBook-Air.local>
2026-02-23 03:59:52 +0300

    Boost connected + new class AuthClient with logic connection

---
+++ client/AuthClient.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Алексей Подоплелов on 23.02.2026.
+//
+
+#include "AuthClient.h"
---
+++ client/AuthClient.h
@@ -0,0 +1,13 @@
+//
+// Created by Алексей Подоплелов on 23.02.2026.
+//
+
+#ifndef CLIENT_AUTHCLIENT_H
+#define CLIENT_AUTHCLIENT_H
+
+
+class AuthClient {
+};
+
+
+#endif //CLIENT_AUTHCLIENT_H
